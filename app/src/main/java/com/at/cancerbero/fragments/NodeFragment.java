e6530a46dc730ebf52a429b4b51ee12f8286774f
Alberto Crespo <albertocresposanchez@gmail.com>
2018-01-15 20:54:11 +0100

    Start with the different tabs

--- app/src/main/java/com/at/cancerbero/fragments/NodeFragment.java
+++ app/src/main/java/com/at/cancerbero/fragments/NodeFragment.java
@@ -2,6 +2,11 @@ package com.at.cancerbero.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.at.cancerbero.CancerberoApp.R;
@@ -19,9 +25,12 @@ import com.at.cancerbero.service.events.Event;
 import com.at.cancerbero.service.events.NodeLoaded;
 import com.at.cancerbero.service.events.ServerError;
 
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
-public class NodeFragment extends AppFragment {
+public class NodeFragment extends AppFragment implements TabLayout.OnTabSelectedListener, ViewPager.OnPageChangeListener {
+
+    private static final String TAB_SELECTED = "TAB_SELECTED";
 
     private TextView nodeName;
 
@@ -32,6 +41,12 @@ public class NodeFragment extends AppFragment {
     private SwipeRefreshLayout swipeRefreshLayout;
 
     private String nodeId;
+
+    private TabLayout tabLayout;
+
+    private ViewPager mViewPager;
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
     public NodeFragment() {
     }
@@ -103,8 +118,8 @@ public class NodeFragment extends AppFragment {
 
         statusImage = view.findViewById(R.id.node_status);
 
-        listView = view.findViewById(R.id.list_pins);
-        registerForContextMenu(listView);
+//        listView = view.findViewById(R.id.list_pins);
+//        registerForContextMenu(listView);
 
         swipeRefreshLayout = view.findViewById(R.id.layout_swipe);
 
@@ -115,20 +130,50 @@ public class NodeFragment extends AppFragment {
             }
         });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getChildFragmentManager());
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                listView.showContextMenuForChild(view);
-            }
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) view.findViewById(R.id.pager_viewer);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        });
+        mViewPager.addOnPageChangeListener(this);
+
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                listView.showContextMenuForChild(view);
+//            }
+//
+//        });
+
+        tabLayout = view.findViewById(R.id.layout_tabs);
+        tabLayout.removeAllTabs();
+        tabLayout.setVisibility(View.VISIBLE);
+        tabLayout.addOnTabSelectedListener(this);
+
+        tabLayout.addTab(createTab(tabLayout, R.string.title_tab_alarm, R.drawable.ic_settings_remote_black_24dp));
+        tabLayout.addTab(createTab(tabLayout, R.string.title_tab_card, R.drawable.ic_payment_black_24dp));
 
         loadNode(view);
 
         getMainActivity().setActivityTitle(R.string.title_node);
 
         return view;
+    }
+
+    private TabLayout.Tab createTab(TabLayout tabLayout, Integer title, Integer icon) {
+        TabLayout.Tab result = tabLayout.newTab();
+
+        if (title != null) {
+            result.setText(title);
+        }
+
+        if (icon != null) {
+            result.setIcon(icon);
+        }
+
+        return result;
     }
 
     private void loadNode(View view) {
@@ -143,12 +188,15 @@ public class NodeFragment extends AppFragment {
         }
     }
 
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         if (nodeId != null) {
             outState.putString("nodeId", nodeId);
         }
+
+        outState.putInt(TAB_SELECTED, tabLayout.getSelectedTabPosition());
     }
 
     @Override
@@ -158,7 +206,23 @@ public class NodeFragment extends AppFragment {
         if((savedInstanceState != null)  && (savedInstanceState.containsKey("nodeId"))){
             nodeId = savedInstanceState.getString("nodeId");
             loadNode(getView());
+
         }
+            int tabIndex = 0;
+
+            if (savedInstanceState != null) {
+                tabIndex = savedInstanceState.getInt(TAB_SELECTED, 0);
+            }
+            selectTab(tabIndex);
+    }
+
+    private void selectTab(int position) {
+        TabLayout.Tab tab = tabLayout.getTabAt(position);
+
+        if (tab != null) {
+            tab.select();
+        }
+
     }
 
 
@@ -166,5 +230,61 @@ public class NodeFragment extends AppFragment {
     public boolean onBackPressed() {
         changeFragment(InstallationFragment.class);
         return true;
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+        mViewPager.setCurrentItem(-1);
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        selectTab(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        List<TabFragment> items = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+            items.add(new TabAlarmFragment());
+            items.add(new TabCardFragment());
+
+            for (TabFragment item : items) {
+                item.setTabsFragment(NodeFragment.this);
+            }
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
     }
 }
